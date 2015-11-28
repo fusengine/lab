7ae53414ba31b3bea892078a1134ce1ae85152dd
Bruno Azoulay <azoulaybruno@gmail.com>
2015-11-28 19:45:50 +0100

    update

--- bootlocal.sh
+++ bootlocal.sh
@@ -1,7 +1,7 @@
 #! Create and mount folder
 sudo mkdir /var/www
-sudo mount -t vboxsf www /var/www
+sudo mount -t vboxsf local.dev /var/www
 
 #! composer command
-echo 'alias composer='\''docker run -ti --rm -v  $(pwd):/app composer/composer'\''' >> /home/docker/.ashrc
+echo 'alias composer='\''docker run -ti --rm -v  $(pwd):/app fusengine/composer'\''' >> /home/docker/.ashrc
 echo 'cd /var/www' >> /home/docker/.ashrc
